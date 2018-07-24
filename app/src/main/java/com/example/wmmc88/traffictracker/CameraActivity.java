0232162a84568d022dd48e1efde51743fbda1ac8
Melvin Wang <melvin.mc.wang@gmail.com>
2018-07-24 16:19:29 -0700

    [BROKEN] Permission Request

--- app/src/main/java/com/example/wmmc88/traffictracker/CameraActivity.java
+++ app/src/main/java/com/example/wmmc88/traffictracker/CameraActivity.java
@@ -1,5 +1,12 @@
 package com.example.wmmc88.traffictracker;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,5 +19,16 @@ public class CameraActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        requestPermissions(this);
+    }
+
+    public static final int CAMERA_PERMISSION_REQUEST = 0;
+
+    public static void requestPermissions(Activity activity) {
+        Log.d(activity.getLocalClassName(), "requestPermissions");
+
+        while (ContextCompat.checkSelfPermission(activity, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST);
+        }
     }
 }
